0acbf32916e26c43ce39cc31519508ddba91b358
archer7717 <128358378+archer7717@users.noreply.github.com>
2024-03-02 17:12:55 +0300

    Add files via upload

---
+++ HC-06/test/test.ino
@@ -0,0 +1,26 @@
+int val;
+int LED = 13;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    val = Serial.read();
+    // При символе "1" включаем светодиод
+    if (val == '1')
+    {
+      digitalWrite(LED, HIGH);
+    }
+    // При символе "0" выключаем светодиод
+    if ( val == '0')
+    {
+      digitalWrite(LED, LOW);
+    }
+  }
+}
