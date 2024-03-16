4f1ee7869e58421c49a23254facfc6156c2a52bc
Михаил <mikhail.povor.77@mail.ru>
2024-03-16 23:12:54 +0300

    bug_programm

--- catamaran3/catamaran3.ino
+++ catamaran3/catamaran3.ino
@@ -1,13 +1,18 @@
 #include <Servo.h>
+char signal;
 int speed;
+int left;
+int right;
 int transmission;
 Servo motor1;
 Servo motor2;
+int speedr;
 
 
 void setup() {
-    motor1.atach(10);
-    motor2.atach(9);
+  
+    motor1.attach(10);
+    motor2.attach(9);
     motor1.writeMicroseconds(800);
     motor2.writeMicroseconds(800);
     delay(5000);
@@ -15,23 +20,62 @@ void setup() {
 
 }
 
-void loop(){
-  if (softSerial.avaible()>0) {
-    status
+void loop()
+{
+  if (Serial.available()>0) {
+     signal = Serial.read();
+    
+     if (isDigit(signal))
+     {
+      transmission = signal;
+       Serial.println(transmission); 
+     }
+    
+    
     
     speed = Serial.read();
     Serial.println(speed);
 
-    speed = map(speed,800,2100);
+    speed = constrain(speed,800,2100);
     
 
-    motor1 = writeMicroseconds(val);
-    motor2 = writeMicroseconds(val);
-    transmission = map(transmission,0,9);
+    motor1.writeMicroseconds(speed);
+    motor2.writeMicroseconds(speed);
+    transmission = constrain(transmission,0, 9);
 
     speed = (transmission * 144) + 800;
-    
+    left = speed;
+    right = speed;
+    speedr = ((transmission - 1) * 144) + 800;
+    if (signal == 'L')
+    {
 
+     motor1 = ((transmission + 1) * 144) + 800;
+     motor2 = speedr;
+      //left;
+      Serial.println("left");
+      }
+    if (signal == 'F')
+    {
+      speed;
+       Serial.println("forward")
+      }
+    
+    if (signal == 'R')
+    {
+      
+      speedr = motor1
+      Serial.println("right");
+      }
+
+    if (signal == 'B')
+    {
+      speed = 0;
+      Serial.println("stop");
+      }
+    
+  motor1.writeMicroseconds(right);
+  motor2.writeMicroseconds(left);
   }
 
 
